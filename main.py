90dadac7c81652f121d1a8a08cb5156950a7898f
Juan <92172817+JuanVillaverdeRodriguez@users.noreply.github.com>
2025-02-23 02:46:44 +0100

    main update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import traceback
 
 app = FastAPI()
 
@@ -10,14 +11,19 @@ def read_root():
 
 @app.get("/scrape")
 def scrape_website(url: str = "https://www.google.com"):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  # Modo sin interfaz gráfica
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")  # Modo sin interfaz gráfica
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver.get(url)
-    title = driver.title
-    driver.quit()
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        driver.get(url)
+        title = driver.title
+        driver.quit()
 
-    return {"url": url, "title": title}
+        return {"url": url, "title": title}
+    
+    except Exception as e:
+        error_message = traceback.format_exc()
+        return {"error": "Ocurrió un error", "details": error_message}
